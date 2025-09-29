687b48c5a66feaedc89167bc6f54e4f41f5ab8f8
Arun Singh Rathore <arun.rathore@olx.in>
2025-09-29 11:42:37 +0530

    first commit

---
+++ Sources/MyLibrary/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  MyLibrary
+//
+//  Created by Arun Rathore  on 29/09/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ Sources/MyLibrary/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView 2.swift
+//  MyLibrary
+//
+//  Created by Arun Rathore  on 29/09/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView_2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView_2()
+}
